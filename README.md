c1f6204f8ae957411b1a14fcd3bf01fe60419c95
PeteJames21 <peetjames21@gmail.com>
2023-10-31 16:51:30 +0300

    Traverse a binary tree in preorder fashion

--- README.md
+++ README.md
@@ -18,3 +18,6 @@ Write a function that checks if a node is a leaf.
 
 ## 5-binary_tree_is_root.c
 Write a function that checks if a given node is a root.
+
+## 6-binary_tree_preorder.c
+Write a function that goes through a binary tree using pre-order traversal.
--- binary_trees.h
+++ binary_trees.h
@@ -28,8 +28,9 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 
-/* Binary tree helper functions */
+/* Functions for operation on the entire tree */
 void binary_tree_delete(binary_tree_t *tree);
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_root(const binary_tree_t *node);
 
