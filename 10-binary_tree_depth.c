f080784818917f1ba32c694280f44214ee6ddf70
PeteJames21 <peetjames21@gmail.com>
2023-11-02 12:13:30 +0300

    Update docstring

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -5,6 +5,8 @@
  * binary_tree_depth - find the depth of a node in a binary tree
  * @tree: a pointer to the node to measure the depth
  * Return: the depth of the node. 0 is returned if the tree is NULL
+ *
+ * Description - the root node is considered to be at depth 0
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
